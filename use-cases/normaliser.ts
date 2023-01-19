5a66b4b9c46d36235d72c5c3a10f6742ac0825cd
ilemuelfigueira <lfigueiradev@gmail.com>
2023-01-19 03:41:33 -0300

    feat: logging normalizer result

--- use-cases/normaliser.ts
+++ use-cases/normaliser.ts
@@ -2,16 +2,22 @@ import * as levenshtein from "./levenshtein-distance";
 import { Logger } from "./logger";
 
 class Normaliser {
-
   private console: Logger;
   constructor() {
     this.console = new Logger("Normaliser");
   }
 
-  normalise(input: string, targets: string[] = ["Software engineer", "Accountant"], targetScore: number = 0.5) {
+  normalise(
+    input: string,
+    targets: string[] = ["Software engineer", "Accountant"],
+    targetScore: number = 0.5
+  ) {
     this.console.debug(`Normalising ${input}`);
 
-    return levenshtein.getMatchByScore(input, targets, 0.5);
+    const result = levenshtein.getMatchByScore(input, targets, 0.5);
+
+    this.console.info(`${result}`);
+    return result;
   }
 }
 
